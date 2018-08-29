3b5ef08bbe322548a2682c30e8197a2d002d57c4
Remco Tolsma <info@remcotolsma.nl>
2018-08-29 16:39:11 +0200

    Updated packages.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,297 +1,71 @@
+/**
+ * Grunt
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2018 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Gateways\OmniKassa2
+ */
+
 module.exports = function( grunt ) {
 	require( 'load-grunt-tasks' )( grunt );
 
-	var phpFiles = [
-		'**/*.php',
-		'!bower_components/**',
-		'!deploy/**',
-		'!node_modules/**',
-		'!vendor/**'
-	];
-
-	// Project configuration
+	// Project configuration.
 	grunt.initConfig( {
-		// Package
+		// Package.
 		pkg: grunt.file.readJSON( 'package.json' ),
 
-		dirs: {
-			ignore: [ 'bower_components', 'deploy', 'node_modules', 'vendor' ].join( ',' ) 
+		// JSHint.
+		jshint: {
+			all: [ 'Gruntfile.js', 'composer.json', 'package.json' ]
 		},
 
-		// PHP Code Sniffer
+		// PHP Code Sniffer.
 		phpcs: {
-			application: {
-				src: phpFiles
-			},
 			options: {
 				bin: 'vendor/bin/phpcs',
 				standard: 'phpcs.xml.dist',
 				showSniffCodes: true
-			}
-		},
-
-		// PHPLint
-		phplint: {
-			options: {
-				phpArgs: {
-					'-lf': null
-				}
 			},
-			all: phpFiles
-		},
-
-		// PHP Mess Detector
-		phpmd: {
 			application: {
-				dir: '.'
-			},
-			options: {
-				exclude: '<%= dirs.ignore %>',
-				reportFormat: 'text',
-				rulesets: 'phpmd.ruleset.xml'
-			}
-		},
-		
-		// Check WordPress version
-		checkwpversion: {
-			options: {
-				readme: 'readme.txt',
-				plugin: 'orbis.php',
-			},
-			check: {
-				version1: 'plugin',
-				version2: 'readme',
-				compare: '=='
-			},
-			check2: {
-				version1: 'plugin',
-				version2: '<%= pkg.version %>',
-				compare: '=='
-			}
-		},
-
-		// Check textdomain errors
-		checktextdomain: {
-			options:{
-				text_domain: '<%= pkg.name %>',
-				keywords: [
-					'__:1,2d',
-					'_e:1,2d',
-					'_x:1,2c,3d',
-					'esc_html__:1,2d',
-					'esc_html_e:1,2d',
-					'esc_html_x:1,2c,3d',
-					'esc_attr__:1,2d',
-					'esc_attr_e:1,2d',
-					'esc_attr_x:1,2c,3d',
-					'_ex:1,2c,3d',
-					'_n:1,2,4d',
-					'_nx:1,2,4c,5d',
-					'_n_noop:1,2,3d',
-					'_nx_noop:1,2,3c,4d'
-				]
-			},
-			files: {
-				src:  [
+				src: [
 					'**/*.php',
-					'!bower_components/**',
-					'!deploy/**',
 					'!node_modules/**',
-					'!vendor/**'
-				],
-				expand: true
-			}
-		},
-		
-		// MakePOT
-		makepot: {
-			target: {
-				options: {
-					domainPath: 'languages',
-					type: 'wp-plugin',
-					updatePoFiles: true,
-					exclude: [ 'bower_components/.*', 'deploy/.*', 'node_modules' ],
-				}
-			}
-		},
-		
-		// Copy
-		copy: {
-			assets: {
-				files: [
-					{ // AngularJS
-						expand: true,
-						cwd: 'node_modules/angular',
-						src: [ 'angular-csp.css', 'angular.js', 'angular.min.js', 'angular.min.js.map' ],
-						dest: 'assets/angular'
-					},
-					{ // AngularJS jQuery UI Datepicker
-						expand: true,
-						cwd: 'node_modules/angular-ui-date/src',
-						src: [ 'date.js' ],
-						dest: 'assets/angular-ui-date'
-					},
-					{ // AngularJS ui-select
-						expand: true,
-						cwd: 'node_modules/angular-ui-select/dist',
-						src: [ 'select.css', 'select.js' ],
-						dest: 'assets/angular-ui-select'
-					},
-					{ // Select2
-						expand: true,
-						cwd: 'node_modules/select2/dist',
-						src: [ '**' ],
-						dest: 'assets/select2'
-					},
+					'!vendor/**',
+					'!wordpress/**',
 				]
 			},
-
-			deploy: {
-				src: [
-					'**',
-					'!bower.json',
-					'!Gruntfile.js',
-					'!package.json',
-					'!phpcs.ruleset.xml',
-					'!phpmd.ruleset.xml',
-					'!bower_components/**',
-					'!deploy/**',
-					'!node_modules/**',
-					'!vendor/**'
-				],
-				dest: 'deploy/latest',
-				expand: true
-			},
 		},
 
-		// Imagemin
-		imagemin: {
-			build: {
-				files: [
-					{ // Images
-						expand: true,
-						cwd: 'src/images/',
-						src: ['**/*.{png,jpg,gif,svg}'],
-						dest: 'images/'
-					}
-				]
-			}
+		// PHPLint.
+		phplint: {
+			all: [ 'src/**/*.php' ]
 		},
 
-		// Clean
-		clean: {
-			build: {
-				src: [
-					'assets'
-				]
-			},
-
-			deploy: {
-				src: [ 'deploy/latest' ]
-			},
-		},
-
-		// Composer
-		composer: {
-			options : {
-				cwd: 'deploy/latest',
-				flags: [
-					'no-dev',
-					'prefer-dist'
-				]
-			}
-		},
-
-		// Compress
-		compress: {
-			deploy: {
-				options: {
-					archive: 'deploy/archives/<%= pkg.name %>.<%= pkg.version %>.zip'
-				},
-				expand: true,
-				cwd: 'deploy/latest',
-				src: ['**/*'],
-				dest: '<%= pkg.name %>/'
-			}
-		},
-
-		// Git checkout
-		gitcheckout: {
-			tag: {
-				options: {
-					branch: 'tags/<%= pkg.version %>'
-				}
-			},
-			develop: {
-				options: {
-					branch: 'develop'
-				}
-			}
-		},
-
-		// S3
-		aws_s3: {
+		// PHP Mess Detector.
+		phpmd: {
 			options: {
-				region: 'eu-central-1'
+				bin: 'vendor/bin/phpmd',
+				exclude: 'node_modules',
+				reportFormat: 'xml',
+				rulesets: 'phpmd.ruleset.xml'
 			},
-			deploy: {
-				options: {
-					bucket: 'downloads.pronamic.eu',
-					differential: true
-				},
-				files: [
-					{
-						expand: true,
-						cwd: 'deploy/archives/',
-						src: '<%= pkg.name %>.<%= pkg.version %>.zip',
-						dest: 'plugins/<%= pkg.name %>/'
-					}
-				]
+			application: {
+				dir: 'src'
 			}
 		},
 
-		// WordPress deploy
-		rt_wp_deploy: {
-			app: {
-				options: {
-					svnUrl: 'http://plugins.svn.wordpress.org/<%= pkg.name %>/',
-					svnDir: 'deploy/wp-svn',
-					svnUsername: 'pronamic',
-					deployDir: 'deploy/latest',
-					version: '<%= pkg.version %>',
-				}
+		// PHPUnit.
+		phpunit: {
+			options: {
+				bin: 'vendor/bin/phpunit'
+			},
+			application: {
+
 			}
-		},
+		}
 	} );
 
 	// Default task(s).
-	grunt.registerTask( 'default', [ 'phplint', 'phpmd', 'phpcs', 'checkwpversion', 'copy:assets', 'imagemin' ] );
-	grunt.registerTask( 'pot', [ 'checktextdomain', 'makepot' ] );
-
-	grunt.registerTask( 'deploy', [
-		'default',
-		'clean:deploy',
-		'copy:deploy',
-		'composer:install',
-		'compress:deploy'
-	] );
-
-	grunt.registerTask( 'wp-deploy', [
-		'gitcheckout:tag',
-		'deploy',
-		'rt_wp_deploy',
-		'gitcheckout:develop'
-	] );
-
-	grunt.registerTask( 's3-deploy', [
-		'gitcheckout:tag',
-		'deploy',
-		'aws_s3:deploy',
-		'gitcheckout:develop'
-	] );
-
-	grunt.registerTask( 'assets', [
-		'clean',
-		'copy'
-	] );
+	grunt.registerTask( 'default', [ 'jshint', 'phplint', 'phpmd', 'phpcs', 'phpunit' ] );
 };
---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Bootstrap tests
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2018 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Extensions\MemberPress
+ */
+
+/**
+ * Autoload.
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
+
+require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
